2639cae75766e30badc0cfd3012ed2ec9d80f44c
Nikolay Moroz <68197237+Adedal513@users.noreply.github.com>
2022-05-15 11:40:44 +0300

    Оптимизировано форматирование дат
    Конструктор даты, вставляемой в URL API теперь состоит из одной строки

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -47,11 +47,9 @@ def fetch_nasa_epic(api_key: str, number_of_photos=1):
         image_data = images_info[photo_num]
 
         image_name = image_data['image']
-        image_date = datetime.strptime(image_data['date'], "%Y-%m-%d %H:%M:%S")
+        image_date = datetime.fromisoformat(image_data['date']).strftime('%Y/%m/%d')
 
-        year, month, day = image_date.year, str(image_date.month).zfill(2), str(image_date.day).zfill(2)
-
-        image_request_url = f'https://api.nasa.gov/EPIC/archive/natural/{year}/{month}/{day}/png/{image_name}.png?'
+        image_request_url = f'https://api.nasa.gov/EPIC/archive/natural/{image_date}/png/{image_name}.png?'
         image_request_url = image_request_url + urlencode(params)
 
         download_image_by_url(target_directory='./images/nasa_epic', url=image_request_url)
